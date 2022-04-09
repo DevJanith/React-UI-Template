25eb75e1a0f39d6c370d3afbfab0c2fd2ecda969
Janith Gamage <janithgamage1.ed@gmail.com>
2022-04-09 16:30:28 +0530

    feat: lsit and datatable

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,7 +1,16 @@
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./single.scss";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+      </div>
+    </div>
+  );
 };
 
 export default Single;
